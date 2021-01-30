2f5bc06533647cc0c64c6487058085f2eb71b8f9
Furds <furdsalpha@gmail.com>
2021-01-30 16:09:32 +0100

    Début Scriptable Object

---
+++ Assets/Scripts/Pierre-Alexandre/ScriptAble_Object/Sword.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sword", menuName = "Sword")]
+public class Sword : ScriptableObject
+{
+    [Header("Basic informmation")]
+    public new string name;
+    public string description;
+
+    [Header("Visuel")]
+    public Sprite artwork;
+
+    [Header("Statistic")]
+    public int attack;
+    public int cooldown;
+
+    public enum Effect {None, Burn, Froze, Poison, Paralize}
+    [Header("Effet de l'arme")]
+    public Effect effet;
+}
